ab6d02c24f8659a2ebda7c4391d23a013cfbbbee
Unknown <alexander_novo@mail.tmcc.edu>
2018-01-28 13:07:17 -0800

    Timing functions

--- src/engine.cpp
+++ src/engine.cpp
@@ -91,7 +91,7 @@ std::string ErrorString(GLenum error) {
 void Engine::Run() {
 	m_running = true;
 	
-	double radius = 0.1;
+	double radius = 1;
 	//double delta = 0.0025;
 	double delta = radius / 1600.0;
 	comp num;
@@ -110,6 +110,17 @@ void Engine::Run() {
 	shader->Initialize();
 	shader->Enable();
 	
+	time_t now = time(NULL);
+	
+	vertices.reserve(vertexDimensions * vertexDimensions);
+	
+	long long numNewtonMethod = 0;
+	long long numCalced = 0;
+	long long numPerCent = vertexDimensions * vertexDimensions / 100;
+	int perCents = 0;
+	long long counter = 0;
+	
+	std::cout << "Calculated: 0%\r" << std::flush;
 	//generate our vertices
 	for(int x = 0; x < vertexDimensions; x ++) {
 		for(int y = 0; y < vertexDimensions; y ++) {
@@ -119,20 +130,30 @@ void Engine::Run() {
 			for(i = 0; i < 20; i++) {
 				prev_num = num;
 				num = newtonMethod(num, &easy, &easyPrime);
+				numNewtonMethod++;
 				if(prev_num == num) {
 					break;
 				}
 			}
+			numCalced++;
 			hsv.r = std::arg(num) * multiplier;
 			hsv.g = std::abs(num) / radius;
 			hsv.b = 1.0;
 			rgb = Graphics::hsv2rgb(hsv);
 			Vertex vert({_num.real() / radius, _num.imag() / radius, 0.0}, rgb);
 			vertices.push_back(vert);
-			
 			//std::cout << _num << " converges to " << num << " which is " << hsv << " or " << rgb << '\n';
+			counter++;
+			if(counter == numPerCent) {
+				perCents++;
+				std::cout << "Calculated: " << perCents << "%\r" << std::flush;
+				counter = 0;
+			}
 		}
 	}
+	
+	std::cout << "Time taken to generate vertices: " << time(NULL) - now << " s" << std::endl;
+	now = time(NULL);
 	
 	//Now generate faces
 	unsigned vertexIndex;
@@ -189,32 +210,13 @@ void Engine::Run() {
 	glDisableVertexAttribArray(1);
 	m_window->Swap();
 	
+	std::cout << "Time taken to render: " << time(NULL) - now << " s" << std::endl;
+	
 	while(m_running) {
 		while (SDL_PollEvent(&m_event) != 0) {
 			eventHandler(m_DT);
 			ImGui_ImplSdlGL3_ProcessEvent(&m_event);
 		}
-		
-		shader->Enable();
-		glClear(GL_DEPTH_BUFFER_BIT);
-		
-		glEnableVertexAttribArray(0);
-		glEnableVertexAttribArray(1);
-		
-		glBindBuffer(GL_ARRAY_BUFFER, VB);
-		//Now send vertices, uvs, and normals
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), nullptr);
-		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *) offsetof(Vertex, color));
-		
-		//Send all our face information
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IB);
-		
-		//Now draw everything
-		glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
-		
-		glDisableVertexAttribArray(0);
-		glDisableVertexAttribArray(1);
-		m_window->Swap();
 		
 		auto error = glGetError();
 		if (error != GL_NO_ERROR) {
@@ -222,29 +224,6 @@ void Engine::Run() {
 			std::cout << "Error initializing OpenGL! " << error << ", " << val << std::endl;
 		}
 	}
-
-//	while (m_running) {
-//		// Update the DT
-//		m_DT = getDT();
-//
-//		while (SDL_PollEvent(&m_event) != 0) {
-//			eventHandler(m_DT);
-//			ImGui_ImplSdlGL3_ProcessEvent(&m_event);
-//		}
-//
-//		// Check the keyboard input
-//		Keyboard(m_DT);
-//
-//		// Update menu options and labels
-//		m_menu->update(m_DT, _ctx.width, _ctx.height);
-//
-//		//Render everything
-//		//m_graphics->Render();
-//		m_menu->render();
-//
-//		// Swap to the Window
-//		m_window->Swap();
-//	}
 
 	ImGui_ImplSdlGL3_Shutdown();
 }
